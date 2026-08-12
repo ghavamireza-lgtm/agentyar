874a1fb22d423841f730fdcb26b5cd9b694929b6
reza <ghavami.reza@gmail.com>
2026-08-12 17:04:55 +0330

    Main Update 2

--- app/api/pricing/route.ts
+++
@@ -1,9 +0,0 @@
-// src/app/api/pricing/route.ts
-import { NextRequest } from 'next/server'
-import { db } from '../../../lib/db'
-import { successResponse } from '../../../lib/utils'
-
-export async function GET(request: NextRequest) {
-  const plans = db.getPricingPlans()
-  return successResponse(plans)
-}
